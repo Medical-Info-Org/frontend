7d222a2bbeb318e4c858cca2374ba8c1aa02fab7
Ryan-Zayne <phoenixastro97@gmail.com>
2024-12-01 16:36:08 +0100

    feat: update to dropzone

--- components/ui/drop-zone/useDropZone.ts
+++ components/ui/drop-zone/useDropZone.ts
@@ -46,7 +46,7 @@ export type DropZoneProps = {
 export type UseDropZoneProps = DropZoneProps & InputProps;
 
 const useDropZone = (props: UseDropZoneProps) => {
-	const inputRef = useRef<HTMLInputElement>(null);
+	const inputRef = useRef<HTMLInputElement | null>(null);
 
 	const {
 		accept,
@@ -60,7 +60,7 @@ const useDropZone = (props: UseDropZoneProps) => {
 		onUpload,
 		onUploadError,
 		onUploadSuccess,
-		ref = inputRef,
+		ref,
 		validationSettings,
 		validator,
 		...restOfInputProps
@@ -126,24 +126,31 @@ const useDropZone = (props: UseDropZoneProps) => {
 		toggleIsDragging(false);
 	};
 
-	const getRenderProps = () =>
-		({
-			acceptedFiles,
-			inputRef: ref as React.RefObject<HTMLInputElement>,
-			isDragging,
-		}) satisfies RenderProps;
+	const getRenderProps = () => ({ acceptedFiles, inputRef, isDragging }) satisfies RenderProps;
 
 	const getChildren = () => (isFunction(children) ? children(getRenderProps()) : children);
 
 	const getRootProps = () => ({
 		className: cnMerge(
-			"relative isolate flex w-full flex-col",
+			"relative isolate flex w-fit flex-col",
 			classNames?.base,
 			isDragging && ["opacity-60", classNames?.activeDragState]
 		),
 		onDragLeave: handleDragLeave,
 		onDragOver: handleDragOver,
 		onDrop: handleFileUpload,
+	});
+
+	const refCallback: React.RefCallback<HTMLInputElement> = useCallbackRef((node) => {
+		inputRef.current = node;
+
+		if (!ref) return;
+
+		if (isFunction(ref)) {
+			return ref(node);
+		}
+
+		(ref as React.MutableRefObject<HTMLInputElement | null>).current = node;
 	});
 
 	const getInputProps = (): InputProps => ({
@@ -157,7 +164,7 @@ const useDropZone = (props: UseDropZoneProps) => {
 			handleFileUpload(event);
 			onChange?.(event);
 		},
-		ref,
+		ref: refCallback,
 		type: "file",
 		...restOfInputProps,
 	});
